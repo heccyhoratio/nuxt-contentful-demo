d7fa5d30a62805edccd6bf8316f60735fa88fbcc
Cloud User <centos@ip-172-31-5-169.eu-west-1.compute.internal>
2018-06-29 08:31:14 +0000

    working auth webhooks

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
 
-shell_exec('yarn generate');
+require('./vendor/autoload.php');
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'No auth provided.';
+    exit;
+} else {
+    $user = getenv('AUTH_USER');
+    $password = getenv('AUTH_PASSWORD');
+echo($user);
+    if ($user == $_SERVER['PHP_AUTH_USER'] && $password == $_SERVER['PHP_AUTH_PW']) {
+        $output = shell_exec('/usr/bin/sudo /var/www/vhosts/nuxt-contentful-demo/node_modules/.bin/nuxt generate 2>&1');
+        echo("<pre>$output</pre>");
+    }
+        echo("No Auth");
+    exit;
+}
+
+
+
+
+
---
+++ indexANOTHER.php
@@ -0,0 +1,3 @@
+<?php
+//echo shell_exec('sudo ls -lsa 2>&1');
+//echo shell_exec('sudo yarn generate 2>&1');
