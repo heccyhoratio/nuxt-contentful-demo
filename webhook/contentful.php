5d203ee9385501a0ff368e4987b08970bf881b0a
Hector Standen <hector.standen@breakthroughmedia.org>
2018-07-03 11:45:11 +0100

    added better webhooks

---
+++ webhook/contentful.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'No auth provided.';
+    exit;
+} else {
+    $user = getenv('AUTH_USER');
+    $password = getenv('AUTH_PASSWORD');
+    if ($user == $_SERVER['PHP_AUTH_USER'] && $password == $_SERVER['PHP_AUTH_PW']) {
+        $output = shell_exec('/usr/bin/sudo git pull 2>&1');
+        echo("<pre>$output</pre>");
+
+        $output = shell_exec('/usr/bin/sudo /var/www/vhosts/nuxt-contentful-demo/node_modules/.bin/nuxt generate 2>&1');
+        echo("<pre>$output</pre>");
+    }
+    exit;
+}
